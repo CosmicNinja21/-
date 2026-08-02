b2b941efc849d534fc37e5f2671a4010cb2acce4
CosmicNinja21 <94486232+CosmicNinja21@users.noreply.github.com>
2026-08-02 12:36:24 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "<:Tiktok:1533509531491893388>", // required by Discord API, not shown in the client
-        state: "",     // this is what people actually see
+        state: "<:Tiktok:1533509531491893388>",     // this is what people actually see
         type: 1,               // Custom
       },
     ],
